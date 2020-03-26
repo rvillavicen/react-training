7842223dd7a5eba3de9f04ad07c7ffe45941cf0a
Raul Villavicencio <raul.villavicencio@buildingconnected.com>
2020-03-26 16:06:16 -0600

    Agregué campo hasAttachments al archivo de mensajes

---
+++ src/components/PreviewMessage/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { MessageContainer, MessageHeader, MessageBody } from './styled'
+
+const PreviewMessage = () => {
+  return(
+    <MessageContainer>
+      <MessageHeader></MessageHeader>
+      <MessageBody></MessageBody>
+    </MessageContainer>
+  )
+}
+
+export default PreviewMessage
---
+++ src/components/PreviewMessage/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const MessageContainer = styled.div`
+  display: flex;
+  position: relative;
+`
+
+export const MessageHeader = styled.div`
+  display: flex;
+`
+
+export const MessageBody = styled.div`
+  display: flex;
+`
